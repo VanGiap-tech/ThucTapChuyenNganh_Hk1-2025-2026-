f8baa0b3479840c1261d72b591b150cff0075580
VanGiap <hoanggiap2k4@gmail.com>
2025-11-09 13:45:07 +0700

    Gộp các file và folder vào ThucTapHK1

---
+++ Projects/DoanThucTapChuyenNganh/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoanThucTapChuyenNganh.Controllers
+{
+
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
